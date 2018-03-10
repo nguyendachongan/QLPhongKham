3b732f594157016009a5b47bfd9c0a005209199a
Nguyễn Đăng Trí <NguyenDangTri@users.noreply.github.com>
2018-03-10 08:09:13 +0700

    Update Project
    - Creatate project, code DAL class, WCF class.

---
+++ Project_QLPK/Project_WcfService/IService1.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+using Project_DAL;
+
+namespace Project_WcfService
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the interface name "IService1" in both code and config file together.
+    [ServiceContract]
+    public interface IService1
+    {
+        // Drug
+        [OperationContract]
+        List<eDrug> getAllDrugs();
+
+        [OperationContract]
+        bool insertDrug(eDrug e);
+
+        [OperationContract]
+        eDrug getOneDrug(int id);
+
+        [OperationContract]
+        bool updateDrug(eDrug e);
+
+        // Account
+
+        [OperationContract]
+        List<Account> getAllAccount();
+
+        [OperationContract]
+        bool insertAccount(Account e);
+
+        [OperationContract]
+        Account getOneAccount(string id);
+
+        [OperationContract]
+        bool updateAccount(Account e);
+
+        // Type of Drug
+
+        [OperationContract]
+        List<TypeOfDrug> getAllTypeDrugs();
+
+        [OperationContract]
+        bool insertTypeDrug(TypeOfDrug e);
+
+        [OperationContract]
+        TypeOfDrug getOneTypeDrug(int id);
+
+        [OperationContract]
+        bool updateTypeDrug(TypeOfDrug e);
+
+        // Parient
+
+        [OperationContract]
+        List<Patient> getAllParient();
+
+        [OperationContract]
+        bool insertParient(Patient e);
+
+        [OperationContract]
+        Patient getOneParient(int id);
+
+        [OperationContract]
+        bool updateParient(Patient e);
+
+        // Prescription
+
+        [OperationContract]
+        List<PrescriptionDetail> getAllPrescriptionDetail();
+
+        [OperationContract]
+        bool insertPrescriptionDetail(PrescriptionDetail e);
+
+        [OperationContract]
+        PrescriptionDetail getOnePrescriptionDetail(int id);
+
+        [OperationContract]
+        bool updatePrescriptionDetail(PrescriptionDetail e);
+
+    }
+
+    [DataContract]
+    public class eDrug
+    {
+        [DataMember]
+        public int DrugID { get; set; }
+        [DataMember]
+        public string Name { get; set; }
+        [DataMember]
+        public string Description { get; set; }
+        [DataMember]
+        public double Price { get; set; }
+        [DataMember]
+        public int TypeID { get; set; }
+
+    }
+}
---
+++ Project_QLPK/Project_WcfService/Service1.svc.cs
@@ -0,0 +1,199 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+using Project_DAL;
+
+namespace Project_WcfService
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "Service1" in code, svc and config file together.
+    // NOTE: In order to launch WCF Test Client for testing this service, please select Service1.svc or Service1.svc.cs at the Solution Explorer and start debugging.
+    public class Service1 : IService1
+    {
+        DrugDAL drugdal = new DrugDAL();
+        AccountDAL accdal = new AccountDAL();
+        PatientDAL patientdal = new PatientDAL();
+        PrescriptionDAL presdal = new PrescriptionDAL();
+        TypeOfDrugDAL typedal = new TypeOfDrugDAL();
+        List<eDrug> IService1.getAllDrugs()
+        {
+            List<eDrug> ls = new List<eDrug>();
+            foreach(Drug dr in drugdal.getAllDrug())
+            {
+                eDrug edr = new eDrug();
+                edr.DrugID = dr.DrugID;
+                edr.Description = dr.Description;
+                edr.Name = dr.Name;
+                edr.Price =Convert.ToDouble(dr.Price);
+                edr.TypeID = dr.TypeID;
+                ls.Add(edr);
+            }
+            return ls;
+            
+        }
+        public bool insertDrug(eDrug e)
+        {
+            try
+            {
+                Drug d = new Drug();
+                d.Description = e.Description;
+                d.DrugID = e.DrugID;
+                d.Name = e.Name;
+                d.Price = Convert.ToDecimal(e.Price);
+                d.TypeID = e.TypeID;
+                drugdal.insertDrug(d);
+                return true;
+            }
+            catch
+            {
+                return false;
+            }
+        }
+
+
+
+
+        public eDrug getOneDrug(int id)
+        {
+            var result= drugdal.getOneDrug(id);
+            eDrug e = new eDrug();
+            e.TypeID = result.TypeID;
+            e.Description = result.Description;
+            e.DrugID = result.DrugID;
+            e.Name = result.Name;
+            e.Price =Convert.ToDouble(result.Price);
+            return e;
+        }
+
+
+        public bool updateDrug(eDrug e)
+        {
+            throw new NotImplementedException();
+        }
+
+        /// <summary>
+        /// account 
+        /// </summary>
+        /// <returns></returns>
+        public List<Account> getAllAccount()
+        {
+            return accdal.getAllAccount();
+        }        
+
+        public bool insertAccount(Account e)
+        {
+            try
+            {
+                accdal.insertAccount(e);
+                return true;
+            }
+            catch{
+                return false;
+            }
+        }
+
+        public Account getOneAccount(string id)
+        {
+            return accdal.getOneAccount(id);
+        }
+
+       
+        public bool updateAccount(Account e)
+        {
+            throw new NotImplementedException();
+        }
+     
+        // Type of drug
+        public List<TypeOfDrug> getAllTypeDrugs()
+        {
+            return typedal.getAllTypeOfDrug();
+        }
+
+        public bool insertTypeDrug(TypeOfDrug e)
+        {
+            try
+            {
+                typedal.insertTypeOfDrug(e);
+                return true;
+            }
+            catch
+            {
+                return false;
+            }
+        }
+
+        public TypeOfDrug getOneTypeDrug(int id)
+        {
+            return typedal.getOneTypeOfDrug(id);
+        }
+
+        public bool updateTypeDrug(TypeOfDrug e)
+        {
+            throw new NotImplementedException();
+        }
+
+        // patient
+
+
+        public List<Patient> getAllParient()
+        {
+            return patientdal.getAllPatient();
+        }
+
+        public bool insertParient(Patient e)
+        {
+            try
+            {
+                patientdal.insertPatient(e);
+                return true;
+            }
+            catch
+            {
+                return false;
+            }
+        }
+
+        public Patient getOneParient(int id)
+        {
+            return patientdal.getOnePatient(id);
+        }
+
+        public bool updateParient(Patient e)
+        {
+            throw new NotImplementedException();
+        }
+
+
+        // prescription
+        public List<PrescriptionDetail> getAllPrescriptionDetail()
+        {
+            return presdal.getAllPrescription();
+        }
+
+        public bool insertPrescriptionDetail(PrescriptionDetail e)
+        {
+            try
+            {
+                presdal.insertPrescription(e);
+                return true;
+            }
+            catch
+            {
+                return false;
+            }
+        }
+
+        public PrescriptionDetail getOnePrescriptionDetail(int id)
+        {
+            return presdal.getOnePrescription(id);
+        }
+
+        public bool updatePrescriptionDetail(PrescriptionDetail e)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
